47418fbd28f110c11858ba3ef8fa315e8545908c
xiaolaba <xiao_laba_cn@yahoo.com>
2020-05-02 15:59:45 +0800

    Add files via upload

---
+++ fc_apk.bat
@@ -0,0 +1,9 @@
+set apk1="APK Export 3.2.4 HTC 8088 Android 5.0.2.apk"
+set apk2="APK Export 3.2.4 samsung GT-S5660 android 2.3.apk"
+
+set apk3="Oscilloscope 5.0.3 samsung GT-S5660 android 2.3.apk"
+set apk4="Oscilloscope.6.1.24 HTC 8088 Android 5.0.2.apk"
+
+fc %apk1% %apk2%
+
+fc %apk3% %apk4%
